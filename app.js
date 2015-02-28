4a6eff85200cb35746d4774b97bc52a669cf44b3
AbrarSyed <sacabrarsyed@gmail.com>
2015-02-28 13:58:34 -0600

    added config stuff

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var http = require('http');
 
+// Configuration stuff
+var nconf = require('nconf');
+nconf.file({ file: 'config.json' }); // load config json
+nconf.argv().env(); // check CI and env args
+
+// mysql connections
+
+
+// HTTP stuff
+var http = require('http');
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Hello Http');
 });
-server.listen(8080);
+server.listen(nconf.get("http:port"));
